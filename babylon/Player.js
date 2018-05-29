9fd6f75676b339cce28fec3b23f9fae7b37afde3
Isaias Pomales <impomales@gmail.com>
2018-05-28 20:12:00 -0400

    playing with colors

--- babylon/Player.js
+++ babylon/Player.js
@@ -18,6 +18,22 @@ export default class Player extends GameObject {
     );
     vertexData.applyToMesh(this);
 
+    const material = new BABYLON.StandardMaterial(
+      "myFirstMaterial",
+      this.getScene()
+    );
+    // color reflected from recieving light.
+    material.diffuseColor = new BABYLON.Color3(1, 0, 0);
+    // bright spot of light on shiny objects.
+    material.specularColor = new BABYLON.Color3(1, 1, 1);
+    material.specularPower = 100;
+    // color reflected from surrounding scene's color.
+    this.getScene().ambientColor = new BABYLON.Color3(0, 0.5, 0);
+    material.ambientColor = new BABYLON.Color3(0, 0.5, 0);
+    // color of object without any light source.
+    material.emissiveColor = BABYLON.Color3.Purple();
+    this.material = material;
+
     const _this = this;
     this.getScene().registerBeforeRender(() => {
       if (_this.position.y < -10) {
